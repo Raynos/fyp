461f3a41bf371506687f03a917980f20190a9f4e
Raynos <raynos2@gmail.com>
2012-04-18 20:02:32 +0100

    be gone

--- src/modules/data-sources/users.js
+++
@@ -1,9 +0,0 @@
-var pd = require("pd"),
-    mongo = require("../lib/mongo"),
-    base = require("./base")
-
-module.exports = pd.extend({}, base, {
-    setup: function () {
-        this.collection = mongo("Users")
-    }
-})
