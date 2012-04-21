800deac5c835bbcf5403d8b1605c40883b70c87a
Raynos <raynos2@gmail.com>
2012-04-21 16:09:39 +0100

    log the builtins or ragequit

--- src/bouncy.js
+++
@@ -1,9 +0,0 @@
-var bouncy = require('bouncy')
-
-bouncy(function (req, bounce) {
-    if (req.headers.host === 'fyp.raynos.org') {
-        bounce(8080)
-    } else {
-        bounce(8081)
-    }
-}).listen(8000)
